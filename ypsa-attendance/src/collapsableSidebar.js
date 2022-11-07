ff2589a17c990b6d1a7375f672153640891e78c7
Johnny Velazquez <jayv2771@gmail.com>
2022-11-06 19:24:12 -0800

    JIRA #25

--- ypsa-attendance/src/collapsableSidebar.js
+++ ypsa-attendance/src/collapsableSidebar.js
@@ -30,4 +30,17 @@ function App(){
     </main>
   </div>
     )
+    {
+      const collapsedClass = "nav--collapsed";
+      const lsKey = "navCollapsed";
+      const nav = document.querySelector(".nav");
+      const navBorder = nav.querySelector(".nav__border");
+      if (localStorage.getItem(lsKey) === "true") {
+        nav.classList.add(collapsedClass);
+      }
+      navBorder.addEventListener("click", () => {
+        nav.classList.toggle(collapsedClass);
+        localStorage.setItem(lsKey, nav.classList.contains(collapsedClass));
+      });
+    }
 }
